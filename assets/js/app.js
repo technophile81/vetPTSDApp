c9f5a0a31574a21fc79babafb535196413f8b040
Brian Witte <brianw0991@gmail.com>
2018-05-15 09:02:25 -0500

    added firebase scripts to html pages, added initialize firebase and facial recognition API to js file.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,98 @@
 
-//Assessment set up
+function logFunction(){
+    console.log("meep");
+};
 
 $(document).ready(function () {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCYNlGC77cCnZMyrMiKKB4TZnx6qApfqP0",
+    authDomain: "vetptsdapp.firebaseapp.com",
+    databaseURL: "https://vetptsdapp.firebaseio.com",
+    projectId: "vetptsdapp",
+    storageBucket: "vetptsdapp.appspot.com",
+    messagingSenderId: "773708000915"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+//facial recognition API
+var urlArray = [];
+var responseArray = [];
+
+
+function processImage() {
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    var subscriptionKey = "b23434428c4d49d8a925cf2c0d434cbc";
+
+    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes": "emotion",
+    };
+
+    // Display the image.
+    var sourceImageUrl = document.getElementById("URLInput").value;
+    // document.querySelector("#sourceImage").src = sourceImageUrl;
+    urlArray.push(sourceImageUrl);
+
+
+
+
+    // Perform the REST API call.
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+
+    .done(function(data) {
+        // Show formatted JSON on webpage.
+       responseArray.push(JSON.stringify(data, null, 2));
+
+        var emotionData = data[0].faceAttributes.emotion;
+        console.log(emotionData)
+
+    database.ref().set({
+        results: emotionData,
+        url: urlArray
+    });
+
+        console.log(responseArray);
+    })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+        alert(errorString);
+    });
+$(document).on("click", "#imgSub", processImage());
+};
+
+$(document).on("click", "#imgSub", logFunction());
+
+//Assessment set up
+
+
 
     //set variable
     var countAssessment = 0;
